d7254ef1082540d0a7c6ca0058f57af9bbc3e1ee
dealig29 <dealig29@gmail.com>
2016-04-12 15:44:22 +0700

    Improve Create prototypes view

--- resources/views/prototypes/create.blade.php
+++ resources/views/prototypes/create.blade.php
@@ -1,47 +1,48 @@
 @extends('layouts.master')
 
-@section("head.title") 
+@section("head.title")
     <title>Create A Prototype</title>
 @stop
 
 
-@section("head.style")  
-   
+@section("head.style")
+
 @stop
 
 @section('body.content')
     <div id="welcome">
-        <div class="container">            
-            {!! Form::open(array('route' => 'prototypes.store', 'method' => 'post', 'role'=>'form')) !!}     
+        <div class="container">
+            {!! Form::open(array('route' => 'prototypes.store', 'method' => 'post', 'role'=>'form')) !!}
 
   			<div class="form-group">
-                {!! Form::number('category_id', 1) !!}
+                {!! Form::number('category_id', 1, array('class' => 'form-control')) !!}
             </div>
 
   			<div class="form-group">
                 {!! Form::label('name', 'Prototype name') !!}
-                {!! Form::text('name') !!}
+                {!! Form::text('name', null, array('class' => 'form-control')) !!}
             </div>
 
   			<div class="form-group">
                 {!! Form::label('value', 'Values') !!}
-                {!! Form::number('value_length', 4) !!}
-                	{!! Form::text('value0', $value=null) !!}
-                	{!! Form::text('value1', $value=null) !!}
-                	{!! Form::text('value2', $value=null) !!}
-                	{!! Form::text('value3', $value=null) !!}
+                {!! Form::number('value_length', 4, array('class' => 'form-control')) !!}
+                  <div class="form-inline">
+                    {!! Form::text('value0', $value=null, array('class' => 'form-control')) !!}
+                  	{!! Form::text('value1', $value=null, array('class' => 'form-control')) !!}
+                  	{!! Form::text('value2', $value=null, array('class' => 'form-control')) !!}
+                  	{!! Form::text('value3', $value=null, array('class' => 'form-control')) !!}
+                  </div>
             </div>
 
-            <div>             
-                {!! Form::label('description', 'Description') !!}   
-                {!! Form::textarea('description', null , ['size' => '30x5']) !!}
+            <div>
+                {!! Form::label('description', 'Description') !!}
+                {!! Form::textarea('description', null, array('class' => 'form-control'), ['size' => '30x5']) !!}
             </div>
 
   			<div class="form-group">
                 {!! Form::label('type', 'Type View') !!}
-                {!! Form::text('type') !!}
+                {!! Form::select('type', array('text' => 'Text', 'check' => 'Checkbox', 'select' => 'Select'), 'text', array('class' => 'form-control')) !!}
             </div>
-            {!! Form::select('size', array('L' => 'Large', 'S' => 'Small')) !!}
 
                 {!! Form::submit('Save', ['class'=>"btn btn-default"]) !!}
 
@@ -51,5 +52,5 @@
 @stop
 
 @section("body.js")
-    
+
 @stop
